bc0a27082215b0ebef79db5403591ff30c286902
何晋升 <hejinsheng@ffcs.cn>
2022-03-16 17:00:20 +0800

    sql编辑器demo

--- src/router/app.router.js
+++ src/router/app.router.js
@@ -6,10 +6,16 @@ const apps = [
     meta: { title: '首页', icon: 'dashboard', affix: true }
   },
   {
-    path: '/demo',
-    name: 'demo',
-    component: () => import('@/views/demo/index.vue'),
-    meta: { title: 'demo', icon: 'dashboard', affix: true }
+    path: '/gridLayoutDemo',
+    name: '拖拽demo',
+    component: () => import('@/views/gridLayoutDemo/index.vue'),
+    meta: { title: '拖拽demo', icon: 'dashboard', affix: true }
+  },
+  {
+    path: '/sqlEditDemo',
+    name: 'sql编辑器demo',
+    component: () => import('@/views/sqlEditDemo/index.vue'),
+    meta: { title: 'sql编辑器demo', icon: 'dashboard', affix: true }
   }
 ]
 export default apps
