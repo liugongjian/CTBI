bd8a7598b93cf3fad0c196f26b8ed78f52366d62
何晋升 <hejinsheng@ffcs.cn>
2022-03-23 18:38:05 +0800

    富文本展示时没有高度导致画布布局失调问题

--- src/components/BI/Common/ComponentBlock/index.vue
+++ src/components/BI/Common/ComponentBlock/index.vue
@@ -23,12 +23,14 @@
     <!-- 当位置选择为图表上方时 -->
     <div
       v-show="$optionUtils.getParameter(option, 'Basic.Mark.show') && $optionUtils.getParameter(option, 'Basic.Mark.position') === 'onChart'"
+      class="rich-text-content"
       v-html="$optionUtils.getParameter(option, 'Basic.Mark.text')"
     />
     <slot v-if="onLoad" />
     <!-- 尾注内容 -->
     <div
       v-show="$optionUtils.getParameter(option, 'Basic.Footer.show')"
+      class="rich-text-content"
       v-html="$optionUtils.getParameter(option, 'Basic.Footer.text')"
     />
   </div>
@@ -80,5 +82,16 @@ export default {
     border: 1px solid #468cff;
     box-shadow: 0 0 1px 1px #468cff !important;
   }
+}
+.rich-text-content {
+  display: flex;
+  flex-direction: column;
+  min-height: 0px; // 防止展示时没有高度导致画布布局失调
+  max-height: 100px;
+  overflow-y: auto;
+  overflow-x: hidden;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+  word-break: break-word;
 }
 </style>
