44980ba939643596958d67dae48da465166f9947
张晨曦 <zhangchx11@chinatelecom.cn>
2022-04-12 16:42:07 +0800

    完成指标看板汇总&分区域

--- src/components/BI/mixins/wordCloudMixins.js
+++ src/components/BI/mixins/wordCloudMixins.js
@@ -1,13 +1,5 @@
-<<<<<<< Updated upstream:src/components/BI/mixins/wordCloudMixins.js
 // 词云图的混入
 import baseMixins from './baseMixins'
 export default {
   mixins: [baseMixins]
 }
-=======
-// 折线图的混入
-// import baseMixins from './baseMixins'
-// export default {
-//   mixins: [baseMixins],
-// }
->>>>>>> Stashed changes:src/components/BI/mixins/linemixins.js
