03272aca62e691c2aeaf284b3e94d9824897b9d1
张晨曦 <zhangchx11@chinatelecom.cn>
2022-04-12 16:41:30 +0800

    完成指标看板汇总&分区域

---
+++ src/components/BI/Charts/KanBanSummary/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div style="width:100%;height:100%;">
+    <div v-if="dataValue" class="kbs-wrap">
+      <ul>
+        <li v-for="(v, i) in dataValue.mainData" :key="i">{{ v }}</li>
+      </ul>
+      <p>{{ dataValue.secondaryData.title }} {{ dataValue.secondaryData.value }}</p>
+    </div>
+    <div v-else>数据为空</div>
+  </div>
+</template>
+
+<script>
+import { getLayoutOptionById } from '@/utils/optionUtils'
+export default {
+  name: 'KanBanSummary',
+  props: {
+    identify: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      storeOption: {},
+      chartOption: {},
+      dataValue: {
+        'time': '201304',
+        mainData: {
+          'title': '利润金额',
+          'value': '1.558万'
+        },
+        secondaryData: {
+          'title': '利润金额_月环比',
+          'value': '-42.31%'
+        }
+      }
+    }
+  },
+  watch: {
+    storeOption: {
+      handler (val) {
+        val.theme.Basic.Title.testShow = val.theme.Basic.TestTitle.testShow
+        if (JSON.stringify(val.dataSource) !== '{}') {
+          this.dataValue = val.dataSource
+          this.getOption()
+        }
+      },
+      deep: true
+    }
+  },
+  mounted () {
+    this.storeOption = getLayoutOptionById(this.identify)
+    this.getOption()
+  },
+  methods: {
+    getOption () {}
+  }
+}
+</script>
+
+<style scoped>
+.kbs-wrap {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+ul > li {
+  list-style: none;
+  text-align: center;
+}
+ul li:nth-child(2) {
+  font-size: 24px;
+}
+</style>
+
