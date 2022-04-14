531ac4ab2795980951a635ed040fc16866b590c6
zozoxingxing <zozozhangyu@163.com>
2022-04-14 20:44:08 +0800

    散点图数据大小

---
+++ src/components/BI/Settings/Slider/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>数据点大小</div>
+    <el-slider
+      v-model="option.symbolSize"
+      :min="option.minSize"
+      :max="option.maxSize"
+      @change="changeValue"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slider',
+  props: {
+    option: {
+      type: Object,
+      default: () => { }
+    }
+  },
+  methods: {
+    changeValue (val) {
+      this.option.symbolSize = val
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
