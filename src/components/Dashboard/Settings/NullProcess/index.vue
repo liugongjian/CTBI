228070d4a61364400fd40c86ca2f60d185a128ac
liugongjian <liugongjianxin@163.com>
2022-04-20 16:45:36 +0800

    'feat:线图功能设置+系列设置'

---
+++ src/components/Dashboard/Settings/NullProcess/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    空值处理
+    <el-radio-group v-model="option.emptyResolve">
+      <el-radio label="skip">直接跨过</el-radio>
+      <el-radio label="zero">置为0,不断开</el-radio>
+      <el-radio label="break">线条断开</el-radio>
+    </el-radio-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NullProcess',
+  components: {},
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data () {
+    return {
+      seriesOption: null
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
