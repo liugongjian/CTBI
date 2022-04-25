85b08f060442c72f27bae5a8fcc8e1769acd2f9c
copyxia <g.zhengjl@ffcs.cn>
2022-04-25 14:57:20 +0800

    系列设置组件

--- src/components/Dashboard/Charts/PercentStackedBarChart/index.vue
+++ src/components/Dashboard/Charts/PercentStackedBarChart/index.vue
@@ -11,8 +11,6 @@
 </template>
 
 <script>
-import { getLayoutOptionById } from '@/utils/optionUtils'
-import { deepClone } from '@/utils/optionUtils'
 import barMixins from '@/components/Dashboard/mixins/barMixins'
 export default {
   name: 'PercentStackedBarChart',
@@ -25,42 +23,8 @@ export default {
   },
   data () {
     return {
-      storeOption: {},
-      chartOption: {},
-      type: 'PercentStackedBarChart', // 图表类型 1.柱图；2.堆积柱状图；3.百分比堆积柱状图
-      dataValue: null,
-      series: []
+      type: 'PercentStackedBarChart' // 图表类型 1.柱图；2.堆积柱状图；3.百分比堆积柱状图
     }
-  },
-  watch: {
-    storeOption: {
-      handler (val) {
-        this.type = val.theme.Basic.ChartType.type
-        val.theme.Basic.Title.testShow = val.theme.Basic.TestTitle.testShow
-        if (JSON.stringify(val.dataSource) !== '{}') {
-          this.dataValue = deepClone(val.dataSource)// 深拷贝，避免修改数据
-          this.getOption()
-        }
-      },
-      deep: true
-    },
-    'storeOption.dataSource': {
-      handler (val) {
-        if (JSON.stringify(val) !== '{}') {
-          this.dataValue = deepClone(val)
-          // 拿到数据中的系列名字
-          this.getSeriesOptions(this.dataValue)
-          // 拿到数据的系列名字 并设置颜色
-          this.getColor(this.dataValue)
-          // 拿到数据中的指标
-          this.getIndicatorOptions(this.dataValue)
-          this.getOption()
-        }
-      }
-    }
-  },
-  mounted () {
-    this.storeOption = getLayoutOptionById(this.identify)
   },
   methods: {
     getOption () {
