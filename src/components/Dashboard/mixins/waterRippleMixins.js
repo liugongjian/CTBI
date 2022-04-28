e7a1d2ab1c13b70802d4eb2291d8936c033b7716
hyzs_fusj <fusj@hua-cloud.com.cn>
2022-04-28 19:40:00 +0800

    看板样式设置

--- src/components/Dashboard/mixins/waterRippleMixins.js
+++ src/components/Dashboard/mixins/waterRippleMixins.js
@@ -1,5 +1,25 @@
 // 水波纹的混入
 import baseMixins from './baseMixins'
+import { colorTheme } from '@/constants/color.js'
 export default {
-  mixins: [baseMixins]
+  mixins: [baseMixins],
+  methods: {
+    // 拿到数据的系列名字 并设置颜色
+    // 最多五条进度条 不考虑默认颜色是否够用
+    getColor (val) {
+      const color = []
+      val.forEach((item, index) => {
+        color.push({ name: item.name, color: colorTheme['defaultColor'][index].value })
+      })
+      this.storeOption.theme.ComponentOption.progressStyle.color = color
+    },
+    // 获取设置目标值
+    getCfgTargetOption (val) {
+      const cfgTargetOption = []
+      val.forEach(item => {
+        cfgTargetOption.push({ name: item.name, value: item.value, fixedVal: item.value, dynamicVal: '', type: 1, aggregation: 1 })
+      })
+      this.storeOption.theme.FunctionalOption.progressConfig.cfgTargetOption = cfgTargetOption
+    }
+  }
 }
