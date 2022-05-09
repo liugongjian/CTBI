9d5e6b6ccd71ca1e5206ad40957d1d46cce5c949
copyxia <g.zhengjl@ffcs.cn>
2022-05-09 09:15:57 +0800

    仪表盘页面初步

---
+++ src/views/dashboardList/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <page-view>
+    <data-set-wrap>
+      123
+    </data-set-wrap>
+  </page-view>
+</template>
+
+<script>
+export default {
+  name: 'DashboardList'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
