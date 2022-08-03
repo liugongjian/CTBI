7892adaf747e422685aa07d1419eca3118d9f40a
孙善鹏 <1044808778@qq.com>
2022-08-03 16:43:49 +0800

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -41,7 +41,7 @@ module.exports = {
       // 配置跨域
       '/api': {
         // 测试环境地址
-        target: 'http://43.142.102.49:888/ctbibackend/',
+        target: 'http://127.0.0.1:7040/',
         // target: 'http://43.142.102.49:888/ctbibackend/',
         // target: 'http://127.0.0.1:7040/',
         changeOrigin: true,
