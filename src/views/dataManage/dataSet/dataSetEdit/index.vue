05635344b2a75d51d9d660ea07161ab54e68b9d7
何晋升 <hejinsheng@ffcs.cn>
2022-09-05 10:42:00 +0800

    修复 仪表盘调整数据集编辑时的返回问题

--- src/views/dataManage/dataSet/dataSetEdit/index.vue
+++ src/views/dataManage/dataSet/dataSetEdit/index.vue
@@ -241,6 +241,8 @@ export default {
       regex: regex,
       // 数据集名称是否错误
       isErrorName: false,
+      // 控制是否已保存过数据集
+      isSaved: false,
       errorInputText: '',
       // 回显&提交的数据
       dataInfo: {
@@ -326,6 +328,9 @@ export default {
   },
   methods: {
     beforeunloadHandler (e) {
+      if (this.isSaved) {
+        return
+      }
       e = e || window.event
       if (e) {
         e.returnValue = '你所做的更改可能未保存。'
@@ -374,6 +379,7 @@ export default {
           }
           this.saveBtnLoading = true
           updateDataSet(body._id, body).then(res => {
+            this.isSaved = true
             this.$message({
               message: '保存成功',
               type: 'success'
@@ -488,9 +494,14 @@ export default {
       this.editLoading = false
     },
     checkExit () {
-      this.$dialog.show('TipDialog', { content: '您还未对此次代码的编辑进行确认，若此时返回，本次编辑内容将不被保存，请问您是否确认返回？', title: '提示' }, () => {
-        this.$router.go(-1)
-      })
+      const mode = this.$route.query.mode
+      if (mode === 'window') {
+        window.close()
+      } else {
+        this.$dialog.show('TipDialog', { content: '您还未对此次代码的编辑进行确认，若此时返回，本次编辑内容将不被保存，请问您是否确认返回？', title: '提示' }, () => {
+          this.$router.go(-1)
+        })
+      }
     },
     handlerToggleContent () {
       // 判断数据是否发生变更
