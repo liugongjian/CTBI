d311bb95528d194389349ed0165c9267029a9cf8
qinruibin <1356278209@qq.com>
2022-10-27 15:29:23 +0800

    fix:来源去向接入数据

--- src/components/Dashboard/Charts/SourceDestinationChart/index.vue
+++ src/components/Dashboard/Charts/SourceDestinationChart/index.vue
@@ -2,32 +2,29 @@
  * @Author: qinruibin
  * @Date: 2022-08-15 16:42:06
  * @LastEditors: qinruibin
- * @LastEditTime: 2022-08-23 17:57:50
+ * @LastEditTime: 2022-10-27 09:50:10
  * @Description:
 -->
 <template>
   <div style="width: 100%; height: 100%">
-    <div
-      v-if="dataValue.directionList"
-      style="width: 100%; height: 100%"
-    >
+    <div v-if="directionList.length" class="source-chart" style="width: 100%; height: 100%">
       <sourceCharts
-        :center-data="dataValue.centerData"
-        :source-list="dataValue.sourceList"
-        :direction-list="dataValue.directionList"
+        :center-data="centerData"
+        :source-list="sourceList"
+        :direction-list="directionList"
       />
     </div>
-    <svg-icon
-      v-else
-      icon-class="chart-empty-san-key"
-      class="chart-empty-svg"
-    />
+    <svg-icon v-else icon-class="chart-empty-san-key" class="chart-empty-svg" />
   </div>
 </template>
 
 <script>
 import baseMixins from '@/components/Dashboard/mixins/baseMixins'
-import { getLayoutOptionById } from '@/utils/optionUtils'
+import {
+  getLayoutOptionById,
+  deepClone,
+  getDataValueById
+} from '@/utils/optionUtils'
 import sourceCharts from './sourceCharts.vue'
 import store from '@/store'
 export default {
@@ -42,79 +39,104 @@ export default {
       default: ''
     }
   },
-  data () {
+  data() {
     return {
       storeOption: {},
       chartOption: {},
-      dataValue: {
-        centerData: '',
-        sourceList: '',
-        directionList: ''
-      }
-      // centerData: {
-      // },
-      // sourceList: [
-      // ],
-      // directionList: [
-      // ]
+      // dataValue: {
+      //   centerData: '',
+      //   sourceList: '',
+      //   directionList: ''
+      // }
+      centerData: [],
+      sourceList: [
+      ],
+      directionList: [
+      ]
     }
   },
   watch: {
     storeOption: {
-      handler (val) {
+      handler(val) {
         console.log(val)
       },
       deep: true
     }
   },
-  mounted () {
+  mounted() {
     this.storeOption = getLayoutOptionById(this.identify)
     this.dataOption = store.state.app.dataOption
   },
   methods: {
     // 图表重绘事件，继承于baseMixins
-    reloadImpl () {
-      this.formatDataValue()
-      // this.dataValue = this.formatData(deepClone(getDataValueById(this.identify)))
+    reloadImpl() {
+      // this.formatDataValue()
+      this.dataValue = this.formatData(deepClone(getDataValueById(this.identify)))
     },
-    formatDataValue () {
-      this.dataValue.centerData = {
-        name: '中心点',
-        num: 24
-      }
-      this.dataValue.sourceList = [
-        {
-          source: '左1',
-          visits: 21,
-          proportion: '87.50',
-          type: 'left'
-        },
-        {
-          source: '左2',
-          visits: 21,
-          proportion: '87.50',
-          type: 'left'
+    formatData (dataValue) {
+      this.centerData = []
+      this.sourceList = []
+      this.directionList = []
+      console.log(dataValue, 'daadad')
+      const type = dataValue.fields.DimensionType.fields[0].displayColumn || dataValue.fields.DimensionType.fields[0].column
+      const name = dataValue.fields.DimensionName.fields[0].displayColumn || dataValue.fields.DimensionName.fields[0].column
+      const center = dataValue.fields.Dimension.fields[0].displayColumn || dataValue.fields.Dimension.fields[0].column
+      const values = dataValue.fields.Measure.fields[0].displayColumn || dataValue.fields.Measure.fields[0].column
+      // const data = dataValue.data
+      dataValue.data.forEach((value, i) => {
+        if (value[type] === 'center') {
+          this.centerData.push({ name: value[center], num: value[values] })
         }
-      ]
-      this.dataValue.directionList = [
-        {
-          source: '右1',
-          visits: 21,
-          proportion: '87.50',
-          type: 'right'
-        },
-        {
-          source: '右2',
-          visits: 21,
-          proportion: '87.50',
-          type: 'right'
+        if (value[type] === 'source') {
+          this.sourceList.push({ name: value[name], value: value[values], type: value[center] })
         }
-      ]
-      console.log(this.dataValue, 'dddddd')
+        if (value[type] === 'goal') {
+          this.directionList.push({ name: value[name], value: value[values], type: value[center] })
+        }
+      })
+      console.log(this.centerData, this.sourceList, this.directionList, 'ccccc')
     }
+    // formatDataValue() {
+    //   this.dataValue.centerData = {
+    //     name: '中心点',
+    //     num: 24
+    //   }
+    //   this.dataValue.sourceList = [
+    //     {
+    //       source: '左1',
+    //       visits: 21,
+    //       proportion: '87.50',
+    //       type: 'left'
+    //     },
+    //     {
+    //       source: '左2',
+    //       visits: 21,
+    //       proportion: '87.50',
+    //       type: 'left'
+    //     }
+    //   ]
+    //   this.dataValue.directionList = [
+    //     {
+    //       source: '右1',
+    //       visits: 21,
+    //       proportion: '87.50',
+    //       type: 'right'
+    //     },
+    //     {
+    //       source: '右2',
+    //       visits: 21,
+    //       proportion: '87.50',
+    //       type: 'right'
+    //     }
+    //   ]
+    //   console.log(this.dataValue, 'dddddd')
+    // }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
+.source-chart{
+  overflow:hidden;
+}
 </style>
